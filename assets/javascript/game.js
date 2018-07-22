106c8bb70907d6746c4b78b92d1d7ce6230fa198
jrbreckenitch <jamesbreckenitch@Jamess-MacBook-Pro.local>
2018-07-22 17:31:53 -0500

    Sunday afternoon progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,19 @@
+// var guessesLeft = document.getElementById("guessesRemaining");
+
+// var lettersGuessed = document.getElementById("userLetters");
+
+// var wins = document.getElementById("userWins");
+
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var wins = 0;
 
 var losses = 0;
 
-var guessesLeft = document.getElementById("guessesRemaining");
+var guessesLeft = 9;
 
-var lettersGuessed = document.getElementById("userLetters");
+var lettersGuessed = [];
+
 
 var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
@@ -16,24 +23,62 @@ console.log(computerChoice);
 document.onkeyup = function(event) {
 
     // Determines which key was pressed.
+    // lettersGuessed = event.key.toLowerCase();
     lettersGuessed.textContent = event.key;
 
-    for (var i = 9; i <= 0; i--) {
-        if (lettersGuessed === computerChoice) {
+    // for (var i = 9; i >= 0; i--) {
+        if (lettersGuessed.textContent === computerChoice) {
+            // console.log(wins++)
             wins++;
+            console.log("You won!")
         }
-        else if (i <= 0) {
-            losses--;
+
+        else if (lettersGuessed.textContent != computerChoice && guessesLeft != 0) {
+            guessesLeft--;
+            lettersGuessed.push(lettersGuessed.textContent);
+        }
+        else if (guessesLeft === 0 && lettersGuessed.textContent != computerChoice) {
+            console.log("You lost!")
+        }
+    // }
+
+
+    var html = 
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + guessesLeft + "</p>" +
+        "<p>Your guesses so far: " + lettersGuessed + "</p>";
+
+    document.querySelector("#scoreboard").innerHTML = html;
+}
+
+
+
+    // for (var i = 9; i >= 0; i--) {
+    //     if (lettersGuessed === computerChoice) {
+    //         wins++;
+    //         document.getElementById("userWins").innerHTML += wins;
+
+            // document.getElementById("userWins").innerHTML += ++1;
+        //     alert("You Won!");
+        // }
+        // else if (i <= 0) {
+        //     // losses--;
+        //     document.getElementById("loss").innerHTML += i;
+        //     document.getElementById("userLetters").innerHTML = lettersGuessed;
+
+            //need to write code to end reset game
 
         // document.getElementById("guessesRemaining");
-        }
-        else (lettersGuessed <= computerChoice) {
-            
-        }
-    }
+// }
+
+        // else (lettersGuessed <= computerChoice) {
+        //     alert("You lost!")
+        // }
+    
   
 
-}
+
 
     // Randomly chooses a choice from the options array. This is the Computer's guess.
     // var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
